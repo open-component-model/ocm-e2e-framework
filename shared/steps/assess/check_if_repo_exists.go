5fcca301a27d46fd172ea0aad19b592723a7534b
Piaras Hoban <phoban01@gmail.com>
2023-04-07 13:03:44 +0100

    add repo exists assessment
    Signed-off-by: Piaras Hoban <phoban01@gmail.com>

---
+++ shared/steps/assess/check_if_repo_exists.go
@@ -0,0 +1,36 @@
+// SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and Gardener contributors.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package assess
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"code.gitea.io/sdk/gitea"
+	"sigs.k8s.io/e2e-framework/pkg/envconf"
+	"sigs.k8s.io/e2e-framework/pkg/features"
+
+	"github.com/open-component-model/ocm-e2e-framework/shared"
+)
+
+// CheckRepoExists adds a check to verify that a repository exists
+func CheckRepoExists(repoName string) features.Func {
+	return func(ctx context.Context, t *testing.T, config *envconf.Config) context.Context {
+		t.Helper()
+
+		gclient, err := gitea.NewClient(shared.BaseURL, gitea.SetToken(shared.TestUserToken))
+		if err != nil {
+			t.Fatal(fmt.Errorf("failed to create gitea client: %w", err))
+		}
+
+		_, _, err = gclient.GetRepo(shared.Owner, repoName)
+		if err != nil {
+			t.Fatal(fmt.Errorf("failed to find expected repository: %w", err))
+		}
+
+		return ctx
+	}
+}
