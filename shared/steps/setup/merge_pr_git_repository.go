adeb402fd54f4aa35dfe19a90adcb237d2d9c1c4
Verma, Shivendu <shivendu.verma@sap.com>
2023-07-28 19:27:37 +0200

    Added PR merge fn

--- shared/steps/setup/merge_pr_git_repository.go
+++ shared/steps/setup/merge_pr_git_repository.go
@@ -5,75 +5,72 @@
 package setup
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
-	"testing"
-
-	"code.gitea.io/sdk/gitea"
+	"io"
+	"net/http"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/features"
+	"testing"
 
 	"github.com/open-component-model/ocm-e2e-framework/shared"
 )
+
+type Payload struct {
+	Do                     string `json:"Do"`
+	MergeMessageField      string `json:"MergeMessageField"`
+	MergeTitleField        string `json:"MergeTitleField"`
+	MergeWhenChecksSucceed bool   `json:"merge_when_checks_succeed"`
+	ForceMerge             bool   `json:"force_merge"`
+}
 
 // MergePullRequest merges a PR
 func MergePullRequest(repoName string, prNumber int) features.Func {
 	return func(ctx context.Context, t *testing.T, config *envconf.Config) context.Context {
 		t.Helper()
-		gclient, err := gitea.NewClient(shared.BaseURL, gitea.SetToken(shared.TestUserToken))
-		if err != nil {
-			t.Fatal(fmt.Errorf("failed to create gitea client: %w", err))
+
+		data := Payload{
+			Do:                     "squash",
+			MergeMessageField:      "PR Merge",
+			MergeTitleField:        "Auto Merge PR with after successful Validation Check",
+			MergeWhenChecksSucceed: false,
+			ForceMerge:             true,
 		}
-		httpResp, response, err := gclient.MergePullRequest(shared.Owner, repoName, int64(prNumber), gitea.MergePullRequestOption{
-			Style:   gitea.MergeStyleMerge,
-			Title:   "PR",
-			Message: "Auto Merge PR with after successful Validation Check",
-		})
-		fmt.Println("http  ", httpResp)
+		payloadBytes, err := json.Marshal(data)
 		if err != nil {
-			t.Fatal(fmt.Errorf("failed to Merge expected PR %s for Repository %s with error: %w", prNumber, repoName, err))
+			t.Fatal(err)
 		}
-		if !httpResp {
-			fmt.Println("http  ", response.Body, response.Status)
+		body := bytes.NewReader(payloadBytes)
+		url := fmt.Sprintf("%s/api/v1/repos/%s/%s/pulls/%d/merge", shared.BaseURL, shared.Owner, repoName, prNumber)
+		req, err := http.NewRequest("POST", url, body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("token %s", shared.TestUserToken))
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
+
+		t.Log(fmt.Sprintf("Request Status: %d %s ", resp.StatusCode, readResponse(resp.Body)))
+
+		if resp.StatusCode != 200 {
+			t.Fatal(fmt.Errorf("failed to Merge expected PR %d for Repository %s with error: %s", prNumber, repoName, readResponse(resp.Body)))
 		}
 
-		prs, resp, err := gclient.ListRepoPullRequests(shared.Owner, repoName, gitea.ListPullRequestsOptions{
-			ListOptions: gitea.ListOptions{},
-			State:       gitea.StateAll,
-		})
-		if err != nil {
-			t.Fatal(fmt.Errorf("failed to Merge expected PR %s for Repository %s with error: %w", prNumber, repoName, err))
-		}
-		for _, obj := range prs {
-			fmt.Println(
-				obj.ID,
-				obj.URL,
-				obj.Index,
-				obj.Title,
-				obj.Body,
-				obj.IsLocked,
-				obj.Comments,
-				obj.HTMLURL,
-				obj.DiffURL,
-				obj.PatchURL,
-				obj.Mergeable,
-				obj.HasMerged,
-				obj.MergeBase,
-				resp.Status)
-		}
-		gclient.SetSudo(shared.Owner)
-		st, response2, err := gclient.MergePullRequest(shared.Owner, repoName, int64(prNumber), gitea.MergePullRequestOption{
-			Style:   gitea.MergeStyleRebaseMerge,
-			Title:   "PR",
-			Message: "Auto Merge PR with after successful Validation Check",
-		})
-		fmt.Println(st, response2.Status)
-		st3, response3, err := gclient.MergePullRequest(shared.Owner, repoName, int64(prNumber), gitea.MergePullRequestOption{
-			Style:   gitea.MergeStyleSquash,
-			Title:   "PR",
-			Message: "Auto Merge PR with after successful Validation Check",
-		})
-		fmt.Println(st3, response3.Status, response3.Body)
 		return ctx
 	}
+}
+
+func readResponse(response io.Reader) string {
+	content, err := io.ReadAll(response)
+	if err != nil {
+		println(err)
+	}
+	return string(content)
 }
