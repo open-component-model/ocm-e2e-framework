262033600f1cff5e8c240e81a0f90bc82e4ddab2
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2023-09-13 10:23:21 +0200

    added dumping repository content

---
+++ shared/steps/teardown/dump_repository_content.go
@@ -0,0 +1,35 @@
+package teardown
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"code.gitea.io/sdk/gitea"
+	"github.com/open-component-model/ocm-e2e-framework/shared"
+	"sigs.k8s.io/e2e-framework/pkg/envconf"
+	"sigs.k8s.io/e2e-framework/pkg/features"
+)
+
+// DumpRepositoryContent dumps all the files in a repository.
+func DumpRepositoryContent(owner, repo string) features.Func {
+	return func(ctx context.Context, t *testing.T, config *envconf.Config) context.Context {
+		t.Helper()
+
+		gclient, err := gitea.NewClient(shared.BaseURL, gitea.SetToken(shared.TestUserToken))
+		if err != nil {
+			t.Fatal(fmt.Errorf("failed to create gitea client: %w", err))
+		}
+
+		r, _, err := gclient.GetTrees(owner, repo, "main", true)
+		if err != nil {
+			t.Fatal(fmt.Errorf("failed to find repo for %s/%s: %w", owner, repo, err))
+		}
+
+		for _, entry := range r.Entries {
+			t.Logf("Type: %s | Path: %s", entry.Type, entry.Path)
+		}
+
+		return ctx
+	}
+}
